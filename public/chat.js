266c6e5557320b8ea7f5138fbe4809a4ffbad63f
Elizabeth <yessenia.eliz.copado@gmail.com>
2024-10-16 13:29:25 -0600

    correcto al 100

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //var socket = io.connect('http://localhost:5000');
-var socket = io.connect('http://3.22.224.21:5000');
+var socket = io.connect('http://3.141.15.28:5000');
 
 var persona = document.getElementById('persona'),
     appChat = document.getElementById('app-chat'),
